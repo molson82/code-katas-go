6ce95264a9846fcf27493db3598f1e246eafd10b
molson82 <wvandebelt82@yahoo.com>
2021-04-20 22:11:14 -0400

    :rocket: start on john and ann sign up kata

---
+++ johnAndAnnSignUp/john-and-ann-sign-up.go
@@ -0,0 +1,18 @@
+package johnAndAnnSignUp
+
+func Ann(n int) []int {
+	// your code
+  return []int{}
+}
+func John(n int) []int {
+	// your code
+  return []int{}
+}
+func SumJohn(n int) int {
+	// your code
+  return 0
+}
+func SumAnn(n int) int {
+	// your code
+  return 0
+}
