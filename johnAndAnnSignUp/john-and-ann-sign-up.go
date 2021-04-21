05925f29ce15b4563598ddbd58374a98025adfd4
molson82 <wvandebelt82@yahoo.com>
2021-04-20 23:00:41 -0400

    :sparkles: started kata

--- johnAndAnnSignUp/john-and-ann-sign-up.go
+++ johnAndAnnSignUp/john-and-ann-sign-up.go
@@ -1,18 +1,44 @@
 package johnAndAnnSignUp
 
+import "fmt"
+
 func Ann(n int) []int {
 	// your code
-  return []int{}
+	var days []int
+	for i := 1; i <= n/2; i++ {
+		days = append(days, i)
+		days = append(days, i)
+	}
+	return days
 }
 func John(n int) []int {
 	// your code
-  return []int{}
+	var days []int
+	for i := 0; i <= (n/2)+1; i++ {
+    if i % 2 == 0 {
+      days = append(days, i)
+		  days = append(days, i)
+    } else {
+      days = append(days, i)
+    }
+		
+	}
+	return days
 }
 func SumJohn(n int) int {
 	// your code
-  return 0
+  var sum int
+  fmt.Printf("DEBUG: %v\n", John(n))
+  for _,v := range John(n) {
+    sum += v
+  }
+	return sum
 }
 func SumAnn(n int) int {
 	// your code
-  return 0
+  var sum int
+  for _,v := range Ann(n) {
+    sum += v
+  }
+	return sum
 }
